0c56c0ae40680e799f80b47e20b568e254713ab3
celsopires1999 <celso.pires1999@gmail.com>
2024-08-17 00:48:33 +0000

    fix: enable configuration through env vars

--- configs/config.go
+++ configs/config.go
@@ -12,20 +12,31 @@ type conf struct {
 }
 
 func LoadConfig(path string, env string) *conf {
-	var cfg *conf
+	var cfg conf
 	viper.AddConfigPath(path)
 	viper.SetConfigName(".env" + env)
 	viper.SetConfigType("dotenv")
 
 	viper.AutomaticEnv()
 
-	err := viper.ReadInConfig()
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			port := viper.Get("PORT")
+			dbConn := viper.Get("DB_CONNECTION")
+			if dbConn == nil || port == nil {
+				log.Fatal("DB_CONNECTION or PORT not found during configuration")
+			}
+			cfg.DBConn = dbConn.(string)
+			cfg.Port = port.(string)
+			return &cfg
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	err := viper.Unmarshal(&cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = viper.Unmarshal(&cfg)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return cfg
+	return &cfg
 }
